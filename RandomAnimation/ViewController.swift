a0e62d194e418ec589e5e9c6407cec5243f37731
Oksenoyt <a.oksenoyt@gmail.com>
2022-08-04 17:03:44 +0400

    added logic for show animation

--- RandomAnimation/ViewController.swift
+++ RandomAnimation/ViewController.swift
@@ -6,14 +6,26 @@
 //
 
 import UIKit
+import SpringAnimation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var animationView: SpringView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func nextAnimation(_ sender: UIButton) {
+        animationView.animation = "\(AnimationPreset.allCases.randomElement() ?? AnimationPreset.wobble)"
+        animationView.curve = "\(AnimationCurve.allCases.randomElement() ?? AnimationCurve.easeInCirc)"
+        animationView.force = CGFloat.random(in: 0.1...2.0)
+        animationView.duration = CGFloat.random(in: 0.1...3.0)
+        animationView.animate()
+        
+        descriptionLabel.text = "options animation:\n\nanimation - \(animationView.animation)\ncurve - \(animationView.curve)\nforce - \(animationView.force)\nduration - \(animationView.duration)"
+    }
 }
 
