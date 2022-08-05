5ee67cc41087e447c478561c59f50a51475f74d7
Oksenoyt <a.oksenoyt@gmail.com>
2022-08-05 12:25:10 +0400

    fix logic for animation

--- RandomAnimation/ViewController.swift
+++ RandomAnimation/ViewController.swift
@@ -9,23 +9,42 @@ import UIKit
 import SpringAnimation
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var animationView: SpringView!
     @IBOutlet weak var descriptionLabel: UILabel!
     
+    @IBOutlet weak var nextAnimationButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        nextAnimationButton.setTitle("run", for: .normal)
+        animationView.layer.cornerRadius = 15
+        nextAnimationButton.layer.cornerRadius = 15
     }
-
+    
+    private var nextAnimation = ""
+    
     @IBAction func nextAnimation(_ sender: UIButton) {
-        animationView.animation = "\(AnimationPreset.allCases.randomElement() ?? AnimationPreset.wobble)"
-        animationView.curve = "\(AnimationCurve.allCases.randomElement() ?? AnimationCurve.easeInCirc)"
+        
+        if nextAnimation == "" {
+            animationView.animation =
+            "\(AnimationPreset.allCases.randomElement() ?? AnimationPreset.wobble)"
+        } else {
+            animationView.animation = "\(nextAnimation)"
+        }
+        
+        animationView.curve =
+        "\(AnimationCurve.allCases.randomElement() ?? AnimationCurve.easeInCirc)"
         animationView.force = CGFloat.random(in: 0.1...2.0)
         animationView.duration = CGFloat.random(in: 0.1...3.0)
         animationView.animate()
         
-        descriptionLabel.text = "options animation:\n\nanimation - \(animationView.animation)\ncurve - \(animationView.curve)\nforce - \(animationView.force)\nduration - \(animationView.duration)"
+        nextAnimation =
+        "\(AnimationPreset.allCases.randomElement() ?? AnimationPreset.wobble)"
+        nextAnimationButton.setTitle(nextAnimation, for: .normal)
+        
+        descriptionLabel.text = String(format:  "animation options:\n\nanimation - \(animationView.animation)\ncurve - \(animationView.curve)\nforce - %.2f\nduration - %.2f", animationView.force, animationView.duration)
     }
 }
 
